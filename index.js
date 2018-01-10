37ff3fdadbd8595fefcd87814415dbac9d0cf522
Miguel Aliaga <miguel@uorboros.com>
2018-01-10 15:39:10 -0500

    adding files

---
+++ index.js
@@ -0,0 +1,29 @@
+'use strict'
+const Fs = require('fs')
+const Path = require('path')
+const {promisify} = require('util')
+const ReadFile = promisify(Fs.readFile)
+const ConfigFilePath = Path.join(__dirname, 'config.json')
+
+module.exports = (cfp = ConfigFilePath) => {
+  return ReadFile(cfp)
+    .then(content => {
+      const env = process.env.NODE_ENV || 'development'
+      content = JSON.parse(content.toString())[env]
+      const keys = Object.keys(content)
+      keys.forEach(key => {
+        process.env[key] = content[key]
+      })
+      return new Promise((resolve, reject) => {
+        resolve({
+          config: content,
+          message: 'config loaded'
+        })
+      })
+    })
+  .catch( e => {
+    return new Promise((resolve, reject) => {
+      reject(e)
+    })
+  })
+} 
